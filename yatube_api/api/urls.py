e813ac242663cd94bcf732b15e0db31aa33a21b2
coherentus <coherentus@gmail.com>
2021-08-02 07:50:11 +0300

    some fix

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,3 @@
-from rest_framework.authtoken import views
 from rest_framework.routers import DefaultRouter
 
 from django.urls import include, path
@@ -13,11 +12,7 @@ router_v1.register(r'posts/(?P<post_id>\d+)/comments',
 router_v1.register('follow', FollowViewSet, basename='follows')
 
 urlpatterns = [
-    # path('v1/api-token-auth/', views.obtain_auth_token),
     path('v1/', include(router_v1.urls)),
-    # Djoser создаст набор необходимых эндпоинтов.
-    # базовые, для управления пользователями в Django:
     path('v1/', include('djoser.urls')),
-    # JWT-эндпоинты, для управления JWT-токенами:
     path('v1/', include('djoser.urls.jwt')),
 ]
